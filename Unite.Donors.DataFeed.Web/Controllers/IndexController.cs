5799cd63385808cb968bbee6de09168ccbf9d1da
Valiantsin Ulasau <valiantsin.ulasau@dkfz-heidelberg.de>
2021-02-04 11:22:51 +0100

    Indexing controller with reindex action.

---
+++ Unite.Donors.DataFeed.Web/Controllers/IndexController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Unite.Data.Entities.Tasks;
+using Unite.Data.Services;
+
+namespace Unite.Donors.DataFeed.Web.Controllers
+{
+    [Route("api/[controller]")]
+    public class IndexController : Controller
+    {
+        private readonly UniteDbContext _database;
+
+        public IndexController(UniteDbContext database, ILogger<IndexController> logger)
+        {
+            _database = database;
+        }
+
+        [HttpPost]
+        public ActionResult Post()
+        {
+            var donors = _database.Donors
+                .Select(donor => donor.Id)
+                .ToArray();
+
+            var tasks = donors
+                .Select(CreateTask)
+                .ToArray();
+
+            _database.DonorIndexingTasks.AddRange(tasks);
+            _database.SaveChanges();
+
+            return Ok();
+        }
+
+        private DonorIndexingTask CreateTask(string donorId)
+        {
+            var task = new DonorIndexingTask()
+            {
+                DonorId = donorId,
+                Date = DateTime.UtcNow
+            };
+
+            return task;
+        }
+    }
+}
