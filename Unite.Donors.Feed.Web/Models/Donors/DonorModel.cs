9c10179845ec2ab6eadb3931cc6dd9c259ef02cd
vulasau <valiantsin.ulasau@gmail.com>
2022-10-21 15:27:07 +0200

    Trimming donor model projects and studies.

--- Unite.Donors.Feed.Web/Models/Donors/DonorModel.cs
+++ Unite.Donors.Feed.Web/Models/Donors/DonorModel.cs
@@ -1,18 +1,23 @@
-﻿using Unite.Data.Extensions;
-
-namespace Unite.Donors.Feed.Web.Services.Donors;
+﻿namespace Unite.Donors.Feed.Web.Services.Donors;
 
 public class DonorModel
 {
     private string _id;
     private bool? _mtaProtected;
+    private string[] _projects;
+    private string[] _studies;
 
     public string Id { get => _id?.Trim(); set => _id = value; }
     public bool? MtaProtected { get => _mtaProtected; set => _mtaProtected = value; }
+    public string[] Projects { get => Trim(_projects); set => _projects = value; }
+    public string[] Studies { get => Trim(_studies); set => _studies = value; }
 
     public ClinicalDataModel ClinicalData { get; set; }
-
     public TreatmentModel[] Treatments { get; set; }
-    public string[] Projects { get; set; }
-    public string[] Studies { get; set; }
+
+
+    private static string[] Trim(string[] array)
+    {
+        return array?.Select(value => value.Trim()).ToArray();
+    }
 }
