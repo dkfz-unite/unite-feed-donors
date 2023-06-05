6cae75070a51f136865408b5ba91b47e48f4ac1b
vulasau <valiantsin.ulasau@gmail.com>
2023-06-05 14:17:38 +0200

    Filtering copy neutral CNVs.

--- Unite.Donors.Indices/Services/DonorIndexCreationService.cs
+++ Unite.Donors.Indices/Services/DonorIndexCreationService.cs
@@ -232,12 +232,12 @@ public class DonorIndexCreationService : IIndexCreationService<DonorIndex>
         var specimenIds = LoadSpecimenIds(donorId);
 
         var ssmIds = LoadVariantIds<SSM.Variant, SSM.VariantOccurrence>(specimenIds);
-        var cnvIds = LoadVariantIds<CNV.Variant, CNV.VariantOccurrence>(specimenIds, occurrence => occurrence.Variant.TypeId != CNV.Enums.CnvType.Neutral);
+        var cnvIds = LoadVariantIds<CNV.Variant, CNV.VariantOccurrence>(specimenIds);
         var svIds = LoadVariantIds<SV.Variant, SV.VariantOccurrence>(specimenIds);
         var ssmGeneIds = LoadGeneIds<SSM.Variant, SSM.AffectedTranscript>(ssmIds);
-        var cnvGeneIds = LoadGeneIds<CNV.Variant, CNV.AffectedTranscript>(ssmIds);
-        var svGeneIds = LoadGeneIds<SV.Variant, SV.AffectedTranscript>(ssmIds);
-        var geneIds = ssmGeneIds.Union(cnvGeneIds).Union(svGeneIds).ToArray();
+        var cnvGeneIds = LoadGeneIds<CNV.Variant, CNV.AffectedTranscript>(cnvIds, affectedTranscript => affectedTranscript.Variant.TypeId != CNV.Enums.CnvType.Neutral);
+        var svGeneIds = LoadGeneIds<SV.Variant, SV.AffectedTranscript>(svIds);
+        var geneIds = Array.Empty<int>().Union(ssmGeneIds).Union(cnvGeneIds).Union(svGeneIds).ToArray();
 
         return new GenomicStats(geneIds.Length, ssmIds.Length, cnvIds.Length, svIds.Length);
     }
@@ -262,15 +262,20 @@ public class DonorIndexCreationService : IIndexCreationService<DonorIndex>
     /// Loads identifiers of genes affected by given variants.
     /// </summary>
     /// <param name="variantIds">Varians identifiers.</param>
+    /// <param name="filter">Affected transcript filter.</param>
     /// <typeparam name="TVariant">Variant type.</typeparam>
     /// <typeparam name="TAffectedTranscript">Variant affected transcript type.</typeparam>
     /// <returns>Array of genes identifiers.</returns>
-    private int[] LoadGeneIds<TVariant, TAffectedTranscript>(long[] variantIds)
+    private int[] LoadGeneIds<TVariant, TAffectedTranscript>(long[] variantIds, Expression<Func<TAffectedTranscript, bool>> filter = null)
         where TVariant : Variant
         where TAffectedTranscript : VariantAffectedFeature<TVariant, Data.Entities.Genome.Transcript>
     {
+        Expression<Func<TAffectedTranscript, bool>> selectorPredicate = (affectedTranscript => variantIds.Contains(affectedTranscript.VariantId));
+        Expression<Func<TAffectedTranscript, bool>> filterPredicate = filter ?? (affectedTranscript => true);
+
         var ids = _dbContext.Set<TAffectedTranscript>()
-            .Where(affectedTranscript => variantIds.Contains(affectedTranscript.VariantId))
+            .Where(selectorPredicate)
+            .Where(filterPredicate)
             .Select(affectedTranscript => affectedTranscript.Feature.GeneId.Value)
             .Distinct()
             .ToArray();
@@ -290,12 +295,12 @@ public class DonorIndexCreationService : IIndexCreationService<DonorIndex>
         where TVariant : Variant
         where TVariantOccurrence : VariantOccurrence<TVariant>
     {
-        Expression<Func<TVariantOccurrence, bool>> specimenPredicate = (occurrence => specimenIds.Contains(occurrence.AnalysedSample.Sample.SpecimenId));
-        Expression<Func<TVariantOccurrence, bool>> variantPredicate = filter ?? (occurrence => true);
+        Expression<Func<TVariantOccurrence, bool>> selectorPredicate = (occurrence => specimenIds.Contains(occurrence.AnalysedSample.Sample.SpecimenId));
+        Expression<Func<TVariantOccurrence, bool>> filterPredicate = filter ?? (occurrence => true);
 
         var ids = _dbContext.Set<TVariantOccurrence>()
-            .Where(specimenPredicate)
-            .Where(variantPredicate)
+            .Where(selectorPredicate)
+            .Where(filterPredicate)
             .Select(occurrence => occurrence.VariantId)
             .Distinct()
             .ToArray();
