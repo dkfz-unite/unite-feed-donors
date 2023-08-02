6b1ebb72804fb7c305685ce8510dbc23bd2f43e2
vulasau <valiantsin.ulasau@gmail.com>
2023-08-02 17:40:22 +0200

    Protecting API with JWT.

--- Unite.Donors.Feed.Web/Program.cs
+++ Unite.Donors.Feed.Web/Program.cs
@@ -10,10 +10,10 @@ builder.Logging.AddConsole();
 
 builder.Services.Configure();
 
+builder.Services.AddCors();
+
 builder.Services.AddAuthentication(options => options.AddJwtAuthenticationOptions())
                 .AddJwtBearer(options => options.AddJwtBearerOptions());
-
-builder.Services.AddAuthorization(options => options.AddAuthorizationOptions());
 
 builder.Services.AddControllers(options => options.AddMvcOptions())
                 .AddJsonOptions(options => options.AddJsonOptions())
