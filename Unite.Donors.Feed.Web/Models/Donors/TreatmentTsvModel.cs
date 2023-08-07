b0571140efdf564c7f5765677c05b73c8f49480e
DGitRich <daniel.rich@gmx.net>
2023-08-07 17:02:39 +0200

    Create TreatmentTsvModel

---
+++ Unite.Donors.Feed.Web/Models/Donors/TreatmentTsvModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Unite.Donors.Feed.Web.Services.Donors;
+
+public class TreatmentTsvModel
+{
+    public string DonorId { get; set; }
+    public string Therapy { get; set; }
+    public string Details { get; set; }
+    public DateTime? StartDate { get; set; }
+    public int? StartDay { get; set; }
+    public DateTime? EndDate { get; set; }
+    public int? DurationDays { get; set; }
+    public string Results { get; set; }
+}
+
