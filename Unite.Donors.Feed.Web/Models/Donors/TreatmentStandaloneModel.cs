bfd1299714111e38c8265efbf146ca2a1071e164
DGitRich <daniel.rich@gmx.net>
2023-08-10 16:38:51 +0200

    Create TreatmentStandaloneModel

---
+++ Unite.Donors.Feed.Web/Models/Donors/TreatmentStandaloneModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Unite.Donors.Feed.Web.Services.Donors;
+
+public class TreatmentStandaloneModel : TreatmentModel
+{
+    private string _donorId;
+
+    public string DonorId { get => _donorId?.Trim(); set => _donorId = value; }
+}
