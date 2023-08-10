70ecb5f6d9b8551f3e43aa2ea9a1d4ff210a207c
DGitRich <daniel.rich@gmx.net>
2023-08-10 16:39:27 +0200

    Create TreatmentStandaloneModelValidator

---
+++ Unite.Donors.Feed.Web/Models/Donors/Validators/TreatmentStandaloneModelValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+
+namespace Unite.Donors.Feed.Web.Services.Donors.Validators;
+
+public class TreatmentStandaloneModelValidator : AbstractValidator<TreatmentStandaloneModel>
+{
+    private readonly TreatmentModelValidator _treatmentModelValidator = new();
+
+    public TreatmentStandaloneModelValidator()
+    {
+        RuleFor(model => model)
+            .SetValidator(_treatmentModelValidator);
+
+        RuleFor(model => model.DonorId)
+            .NotEmpty()
+            .WithMessage("Should not be empty");
+    }
+}
+
