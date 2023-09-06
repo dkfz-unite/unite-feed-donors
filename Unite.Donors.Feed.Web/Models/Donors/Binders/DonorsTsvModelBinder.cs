c4fd361921b0174d1b27d73bc38822d96fb6d17c
DGitRich <daniel.rich@gmx.net>
2023-09-06 10:30:46 +0200

    Create DonorsTsvModelBinder

---
+++ Unite.Donors.Feed.Web/Models/Donors/Binders/DonorsTsvModelBinder.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Unite.Essentials.Tsv;
+
+namespace Unite.Donors.Feed.Web.Models.Donors.Binders;
+
+public class DonorsTsvModelBinder : IModelBinder
+{
+    public async Task BindModelAsync(ModelBindingContext bindingContext)
+    {
+        if (bindingContext == null)
+            throw new ArgumentNullException(nameof(bindingContext));
+
+        using var reader = new StreamReader(bindingContext.HttpContext.Request.Body);
+
+        var tsv = await reader.ReadToEndAsync();
+
+        var map = new ClassMap<DonorModel>()
+            .Map(entity => entity.Id, "id")
+            .Map(entity => entity.MtaProtected, "mta_protected")
+            .Map(entity => entity.ClinicalData.Diagnosis, "diagnosis");
+
+        var model = TsvReader.Read(tsv, map).ToArray();
+
+        bindingContext.Result = ModelBindingResult.Success(model);
+    }
+}
+
