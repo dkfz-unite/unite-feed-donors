a5fc2ff3e73553872dc964487ffec01e27f48041
DGitRich <daniel.rich@gmx.net>
2023-09-06 10:31:21 +0200

    Create TreatmentStandaloneModelConverter

---
+++ Unite.Donors.Feed.Web/Models/Donors/Converters/TreatmentStandaloneModelConverter.cs
@@ -0,0 +1,28 @@
+﻿
+using Unite.Donors.Feed.Web.Models.Donors;
+
+namespace Unite.Donors.Feed.Web.Models.Converters;
+
+public class TreatmentStandaloneModelConverter
+{
+    public TreatmentStandaloneModel Convert(TreatmentTsvModel source)
+    {
+        var donorModel = new TreatmentStandaloneModel();
+
+        Map(source, donorModel);
+
+        return donorModel;
+    }
+
+    private static void Map(TreatmentTsvModel source, TreatmentStandaloneModel target)
+    {
+        target.DonorId = source.DonorId;
+        target.Therapy = source.Therapy;
+        target.Details = source.Details;
+        target.StartDate = source.StartDate;
+        target.StartDay = source.StartDay;
+        target.EndDate = source.EndDate;
+        target.DurationDays = source.DurationDays;
+        target.Results = source.Results;
+    }
+}
