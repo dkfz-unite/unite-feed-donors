4e891c8dd3eb76a08c8f2d9fc1a122c5de2d303a
DGitRich <daniel.rich@gmx.net>
2023-09-29 14:32:56 +0200

    Update DonorModel

--- Unite.Donors.Feed.Web/Models/Donors/DonorModel.cs
+++ Unite.Donors.Feed.Web/Models/Donors/DonorModel.cs
@@ -13,12 +13,14 @@ public class DonorModel
     public string Id { get => _id?.Trim(); set => _id = value; }
     [JsonPropertyName("mta")]
     public bool? MtaProtected { get => _mtaProtected; set => _mtaProtected = value; }
-    [JsonPropertyName("work_packages")]
+    [JsonPropertyName("projects")]
     public string[] Projects { get => Trim(_projects); set => _projects = value; }
     [JsonPropertyName("studies")]
     public string[] Studies { get => Trim(_studies); set => _studies = value; }
 
+    [JsonPropertyName("clinical_data")]
     public ClinicalDataModel ClinicalData { get; set; }
+    [JsonPropertyName("treatments")]
     public TreatmentModel[] Treatments { get; set; }
 
     private static string[] Trim(string[] array)
