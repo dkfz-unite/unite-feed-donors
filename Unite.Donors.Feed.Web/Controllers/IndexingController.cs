cc3d7f829a855f228e36a38e29afee18722949c6
vulasau <valiantsin.ulasau@gmail.com>
2025-03-27 16:04:31 +0100

    Updated packages and reindexing rules.

--- Unite.Donors.Feed.Web/Controllers/IndexingController.cs
+++ Unite.Donors.Feed.Web/Controllers/IndexingController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Unite.Donors.Feed.Web.Configuration.Constants;
 using Unite.Donors.Feed.Web.Services;
+using Unite.Indices.Context;
 
 namespace Unite.Donors.Feed.Web.Controllers;
 
@@ -9,13 +10,19 @@ namespace Unite.Donors.Feed.Web.Controllers;
 [Authorize(Policy = Policies.Data.Writer)]
 public class IndexingController : Controller
 {
+    private readonly IIndexService<Unite.Indices.Entities.Donors.DonorIndex> _donorIndexService;
+    private readonly IIndexService<Unite.Indices.Entities.Projects.ProjectIndex> _projectIndexService;
     private readonly ProjectIndexingTasksService _projectTasksService;
     private readonly DonorIndexingTasksService _donorTasksService;
 
     public IndexingController(
+        IIndexService<Unite.Indices.Entities.Donors.DonorIndex> donorIndexService,
+        IIndexService<Unite.Indices.Entities.Projects.ProjectIndex> projectIndexService,
         ProjectIndexingTasksService projectTasksService,
         DonorIndexingTasksService donorTasksService)
     {
+        _donorIndexService = donorIndexService;
+        _projectIndexService = projectIndexService;
         _projectTasksService = projectTasksService;
         _donorTasksService = donorTasksService;
     }
@@ -23,6 +30,7 @@ public class IndexingController : Controller
     [HttpPost("projects")]
     public IActionResult PostProjects()
     {
+        _projectIndexService.DeleteIndex().Wait();
         _projectTasksService.CreateTasks();
 
         return Ok();
@@ -31,6 +39,7 @@ public class IndexingController : Controller
     [HttpPost("donors")]
     public IActionResult Post()
     {
+        _donorIndexService.DeleteIndex().Wait();
         _donorTasksService.CreateTasks();
 
         return Ok();
