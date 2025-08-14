48cf792d2c5f40f685fc1ca8e222b05eed7f3639
vulasau <valiantsin.ulasau@gmail.com>
2025-08-14 16:10:12 +0200

    Changed how indexing service is waiting.

--- Unite.Donors.Feed.Web/Handlers/DonorsIndexingHandler.cs
+++ Unite.Donors.Feed.Web/Handlers/DonorsIndexingHandler.cs
@@ -42,16 +42,16 @@ public class DonorsIndexingHandler
 
     private async Task ProcessDonorIndexingTasks(int bucketSize)
     {
+        if (_taskProcessingService.HasTasks(WorkerType.Submission) || _taskProcessingService.HasTasks(WorkerType.Annotation))
+        {
+            _logger.LogInformation("Waiting for other tasks to complete before indexing donors.");
+            return;
+        }
+        
         var stopwatch = new Stopwatch();
 
         await _taskProcessingService.Process(IndexingTaskType.Donor, bucketSize, async (tasks) =>
         {
-            if (_taskProcessingService.HasTasks(WorkerType.Submission) || _taskProcessingService.HasTasks(WorkerType.Annotation))
-            {
-                _logger.LogInformation("Waiting for other tasks to complete before indexing donors.");
-                return false;
-            }
-
             stopwatch.Restart();
 
             var indicesToDelete = new List<string>();
